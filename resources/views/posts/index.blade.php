70c77a5107e8f7d1b8789d4414ff216bca45899f
Bagus Cahyono <baguscyy@gmail.com>
2023-11-25 01:38:07 +0700

    feat:all posts�

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+
+@section('title', 'All Post')
+
+@section('content')
+    <div class="container">
+        <h3>All Post </h3> <span>Total : {{$total_post}}</span>
+        @foreach ($posts as $post)
+        <div class="card mb-3">
+        <div class="card-body">
+        <h5 class="card-title">{{$post->title}}</h5>
+        <p class="card-text">{{$post->content}}</p>
+            <p class="card-text"><small class="text-body-secondary">{{$post->updated_at->format('d F Y H:i')}}</small></p>
+            <a href="#" class="btn btn-success">Selengkapnya</a>
+        </div>
+    </div>
+        @endforeach
+        {{$posts->links('pagination::bootstrap-4')}}
+</div>
+@endsection
